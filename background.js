175138a08d6d0c605c8072e104e9cc0d839eb83e
777388 <96343159+777388@users.noreply.github.com>
2023-08-22 16:08:28 -0700

    Update background.js

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "Link") {
     const selectedText = info.selectionText;
-    const archiveUrl = `${selectedText}`
+    const archiveUrl = `https://${selectedText}`;
     chrome.tabs.create({ url: archiveUrl });
   }
 });
